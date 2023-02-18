61bca0b79f07f22e68de67e8b8ea22c299280bde
Edgars Lejnieks <elejnieks867@gmail.com>
2023-02-18 11:54:10 +0200

    First commit: Added comments, got confused.

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 
 from collections import namedtuple
 
-Bracket = namedtuple("Bracket", ["char", "position"])
+Bracket = namedtuple("Bracket", ["char", "position"]) #kautkāds masīvs laikam
 
-
+'''
 def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
+    return (left + right) in ["()", "[]", "{}"] 
+'''
+#man nav ne jausmas kas šis ir ^^^^^
+
+# 'pass' novērš tukšas funkcijas kļūdu vai kautkas tamlīdzīgs... ?
 
 
 def find_mismatch(text):
@@ -14,6 +18,7 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            
             pass
 
         if next in ")]}":
