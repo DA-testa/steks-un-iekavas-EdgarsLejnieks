8d4544a365f2cbab614ec32c5b59a33097ea5506
Edgars Lejnieks <elejnieks867@gmail.com>
2023-02-19 14:33:20 +0200

    checking if i havent accidentally finished, lol

--- main.py
+++ main.py
@@ -1,53 +1,24 @@
 # python3
 # 221RDB168 Edgars Lejnieks 7. grupa
 
-"""
-First priority is to find the first unmatched closing bracket which either doesn’t 
-have an opening bracket before it, like ] in ](), 
-or closes the wrong opening bracket, like } in ()[}
-
-If there are no such mistakes, then it should 
-find the first unmatched opening bracket without the 
-corresponding closing bracket after it, like ( in {}([].
-
-all brackets in the code should be divided into pairs of matching brackets, 
-such that in each pair the opening bracket goes before the closing bracket, 
-and for any two pairs of brackets either one of them is nested inside 
-another one as in (foo[bar]) or they are separate as in f(a,b)-g[c]. 
-The bracket [ corresponds to the bracket ], { corresponds to }, and ( corresponds to ).
-
-Input contains one string 𝑆 which consists of big and small latin letters, digits, 
-punctuation marks and brackets from the set []{}(). 
- 
-Constraints: The length of 𝑆 is at least 1 and at most 105. 
-Output Format: If the code in 𝑆 uses brackets correctly, output “Success" (without the quotes). 
-Otherwise, output the 1-based index of the first unmatched closing bracket,
-if there are no unmatched closing brackets, output the 1-based index of the first unmatched opening bracket.
-"""
-
 from collections import namedtuple
 
-'''
-Bracket = namedtuple("Bracket", ["char", "position"])
-
-
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"] 
-'''
-#not used because i uderstood what it does only after figuring out how to do it my way.
-#oops.
-
-
 def find_mismatch(text):
+    
     opening_brackets_stack = [] #opening brackets are placed in a stack
+    
     pairsdictionary = { 
         ")":"(",
         "]":"[",        #dictionary that provides the asymmetrical opposite to each opening bracket
         "}":"{"
     }
-    global borat        #borat will tell us whether everything checks out
-    borat = False   
-    for i, next in enumerate(text):
+    
+    
+    global check        #check will tell us whether brackets are matched
+    check = False
+    
+    global index        #index will tell us which bracket is unopened or unmatched(not closed)
+    for index, next in enumerate(text):
         
         if next in "([{": # Process opening bracket
             opening_brackets_stack.append(next) #place opening bracket in stack
@@ -57,63 +28,47 @@ def find_mismatch(text):
             cont = False
             
             if next == ")":
-                for j, next in enumerate(reversed(opening_brackets_stack)): 
-                    checkbracketfromstack = opening_brackets_stack[j]
-                    if checkbracketfromstack == pairsdictionary[")"]:
-                        opening_brackets_stack.pop(j)
+                stacklenght = len(opening_brackets_stack) - 1
+                checkbracketfromstack = opening_brackets_stack[stacklenght]
+                if checkbracketfromstack == pairsdictionary[")"]:
+                        opening_brackets_stack.pop(stacklenght)
                         cont = True
-                        break
                 if cont == False:
-                    borat = False
+                    check = False
                     return
-                    
+                
+            if next == "]":
+                stacklenght = len(opening_brackets_stack) - 1
+                checkbracketfromstack = opening_brackets_stack[stacklenght]
+                if checkbracketfromstack == pairsdictionary["]"]:
+                        opening_brackets_stack.pop(stacklenght)
+                        cont = True
+                if cont == False:
+                    check = False
+                    return
             
-            elif next == "]":
-                for j, next in enumerate(reversed(opening_brackets_stack)): 
-                    checkbracketfromstack = opening_brackets_stack[j]
-                    if checkbracketfromstack == pairsdictionary["]"]:
-                        opening_brackets_stack.pop(j)
+            if next == "}":
+                stacklenght = len(opening_brackets_stack) - 1
+                checkbracketfromstack = opening_brackets_stack[stacklenght]
+                if checkbracketfromstack == pairsdictionary["}"]:
+                        opening_brackets_stack.pop(stacklenght)
                         cont = True
-                        break
                 if cont == False:
-                    borat = False
+                    check = False
                     return
-
-                    
-
-            elif next == "}":
-                for j, next in enumerate(reversed(opening_brackets_stack)): 
-                    checkbracketfromstack = opening_brackets_stack[j]
-                    if checkbracketfromstack == pairsdictionary["}"]:
-                        opening_brackets_stack.pop(j)
-                        cont = True
-                        break
-                if cont == False:
-                    borat = False
-                    return
-                    
 
     if len(opening_brackets_stack) == 0: 
-        borat = True
-
-
+        check = True
 
 def main():
     print("Input text (preferably with brackets): ")
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer ---------------------
-    if borat == True:
+    if check == True:
         print("Success")
     else:
-        print("uh oh")
-        
-    # TODO: make this return position of error
-
+        print(index+1)
 
 if __name__ == "__main__":
     main()
-
-#current bugs
-# ) <- success
-# ()) <- success
